e88414baa305fcb7631d4aeb095629e5528835e7
Ryan Kruger <rkruger@evtcorp.com>
2020-12-07 14:25:38 -0800

    changed module name

--- main.tf
+++ main.tf
@@ -1,9 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
 provider "aws" {
   region = var.region
 }
 
+
+
 module "s3-webapp" {
-  source  = "app.terraform.io/hashicorp-learn/s3-webapp/aws"
+  source  = "app.terraform.io/test2-org/s3-webapp/aws"
   name        = var.name
   region = var.region
   prefix = var.prefix
